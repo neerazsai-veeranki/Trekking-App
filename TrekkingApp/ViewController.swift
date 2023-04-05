8a54a9b57baee54cf8f1dbada3e8685796dc1912
neerazsai veeranki <s549929@nwmissouri.edu>
2023-04-05 13:19:36 -0500

    Initial Commit

---
+++ TrekkingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrekkingApp
+//
+//  Created by Veeranki,Neerazsai on 4/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
